47c1e59e99c5537432a71245d3c83c71530fd6b2
a7mdNasrr <56299213+a7mdNasrr@users.noreply.github.com>
2023-06-17 14:47:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-    const symbol = req.body.queryResult.parameters['symbol'];
+    // Get the stock symbol from the user request
+    const stockSymbol = req.body.queryResult.parameters['Stock'];
+
+    // Set the API key for the Alpha Vantage API
     const apiKey = 'GS9NB1XP4WAP8TNS';
 
-    const url = `https://api.example.com/stockprice?symbol=${symbol}&apikey=${apiKey}`;
+    // Construct the URL to call the Alpha Vantage API
+    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${apiKey}`;
 
+    // Call the Alpha Vantage API to get the stock price information
     request(url, (error, response, body) => {
+        // Handle errors by returning an error response to Dialogflow
         if (error) {
             console.error('Error:', error);
             return res.json({
@@ -20,19 +26,37 @@ app.post('/webhook', (req, res) => {
             });
         }
 
+        // Parse the response body as JSON
         const data = JSON.parse(body);
 
-        if (!data['price']) {
+        // Check if the response contains any errors
+        if (data['Error Message']) {
             return res.json({
-                fulfillmentText: `Sorry, I could not find any information for ${symbol}. Please try a different symbol.`
+                fulfillmentText: `Sorry, I could not find any information for ${stockSymbol}. Please try a different symbol.`
             });
         }
 
-        const price = data['price'];
-        const companyName = data['companyName'];
+        // Get the stock price and other information from the response
+        const stockPrice = data['Global Quote']['05. price'];
+        const companyName = data['Global Quote']['01. symbol'];
 
+        // Generate the personalized response using the user input
+        const responseText = `The current stock price for ${companyName} (${stockSymbol}) is ${stockPrice} dollars`;
+
+        // Construct the custom payload response with the stock information
+        const payload = {
+            type: 'stock',
+            symbol: stockSymbol,
+            price: stockPrice,
+            change: null,
+            chart_url: `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${apiKey}`,
+            chart_alt_text: `Price chart for ${companyName} (${stockSymbol})`
+        };
+
+        // Return the response to Dialogflow with the custom payload
         return res.json({
-            fulfillmentText: `The current price of ${companyName} (${symbol}) is ${price} dollars.`
+            fulfillmentText: responseText,
+            payload: payload
         });
     });
 });
